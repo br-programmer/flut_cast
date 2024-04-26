e249344d4dc648fced77c98a3d4ba7b2212d8d06
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-04-25 22:24:04 -0500

    ♻️ (environment.dart): refactor Environment class to remove hardcoded default values for apiKey, baseUrl, and imageUrl properties to allow for dynamic configuration at runtime

--- lib/core/injector/environment.dart
+++ lib/core/injector/environment.dart
@@ -6,16 +6,7 @@ enum Environment {
   const Environment();
 
   bool get enableLogging => this != Environment.prod;
-  String get apiKey => const String.fromEnvironment(
-        'apiKey',
-        defaultValue: 'f26b3b02fb1f685997074a5143b82e44',
-      );
-  String get baseUrl => const String.fromEnvironment(
-        'baseUrl',
-        defaultValue: 'http://api.openweathermap.org',
-      );
-  String get imageUrl => const String.fromEnvironment(
-        'imageUrl',
-        defaultValue: 'https://openweathermap.org/img/wn',
-      );
+  String get apiKey => const String.fromEnvironment('apiKey');
+  String get baseUrl => const String.fromEnvironment('baseUrl');
+  String get imageUrl => const String.fromEnvironment('imageUrl');
 }
