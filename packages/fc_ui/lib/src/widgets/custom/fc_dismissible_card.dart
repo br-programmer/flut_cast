badb5b23c8127fb356dea8bc667dd7c6f34670b0
Brayan Cantos <brayancantosm07ci@gmail.com>
2024-04-26 23:38:23 -0500

    🔧 (fc_dismissible_card.dart): add support for custom background widget in FCDismissibleCard widget to allow customization of the card's background appearance

--- packages/fc_ui/lib/src/widgets/custom/fc_dismissible_card.dart
+++ packages/fc_ui/lib/src/widgets/custom/fc_dismissible_card.dart
@@ -9,33 +9,40 @@ class FCDismissibleCard extends StatelessWidget {
     required this.onDeleted,
     required this.dismissibleKey,
     required this.child,
+    this.background,
   });
   final VoidCallback onPressed;
   final Future<bool?> Function() onDeleted;
   final Key dismissibleKey;
   final Widget child;
+  final Widget? background;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: borderRadius20,
       child: FCGradient(
-        child: DismissibleTile(
-          rtlDismissedColor: Colors.red,
-          confirmDismiss: (direction) {
-            if (direction == DismissibleTileDirection.leftToRight) {
-              return Future.value();
-            }
-            return onDeleted.call();
-          },
-          key: dismissibleKey,
-          child: InkWell(
-            onTap: onPressed,
-            child: Padding(
-              padding: edgeInsetsH20.add(edgeInsetsV12),
-              child: child,
+        child: Stack(
+          children: [
+            if (background != null) background!,
+            DismissibleTile(
+              rtlDismissedColor: Colors.red,
+              confirmDismiss: (direction) {
+                if (direction == DismissibleTileDirection.leftToRight) {
+                  return Future.value();
+                }
+                return onDeleted.call();
+              },
+              key: dismissibleKey,
+              child: InkWell(
+                onTap: onPressed,
+                child: Padding(
+                  padding: edgeInsetsH20.add(edgeInsetsV12),
+                  child: child,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
